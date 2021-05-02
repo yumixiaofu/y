bbad51644db798933768f1948933ed37cdbb2549
xiangshang <603060475@qq.com>
2021-05-02 23:20:56 +0800

    Update qkmg.js

--- static/player/qkmg.js
+++ static/player/qkmg.js
@@ -1,1 +1,2 @@
-
+MacPlayer.Html = '<iframe src="https://jx.xmflv.com/?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player"  allowfullscreen="true"></iframe>';
+MacPlayer.Show();
