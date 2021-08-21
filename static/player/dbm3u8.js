3c299387a5118bff244e4f4d180034082bef6c96
xiangshang <603060475@qq.com>
2021-08-21 11:12:50 +0800

    Update dbm3u8.js

--- static/player/dbm3u8.js
+++ static/player/dbm3u8.js
@@ -1,2 +1,2 @@
-MacPlayer.Html = '<iframe src="https://www.sjwtv.com/jiexi/bdzy.php?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
+MacPlayer.Html = '<iframe src="/jiexi/bdzy.php?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
 MacPlayer.Show();
